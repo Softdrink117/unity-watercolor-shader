081306e85de225ae6b696d4d323bec50ed1f165e
Softdrink117 <tobyhein@gmail.com>
2017-06-21 14:44:38 -0700

    Initial Upload

---
+++ Assets/Softdrink/WatercolorShader/Watercolor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+ namespace Softdrink{
+
+	[ExecuteInEditMode]
+	[RequireComponent(typeof(Camera))]
+	[AddComponentMenu("Image Effects/Softdrink/Watercolor")]
+	public class Watercolor : MonoBehaviour {
+	 
+		public float intensity;
+		private Material material;
+	 
+		void Awake ()
+		{
+			material = new Material( Shader.Find("Hidden/waterColor_pp") );
+		}
+
+		void SetMaterialProperties(){
+
+		}
+		
+		void OnRenderImage (RenderTexture source, RenderTexture destination)
+		{
+			if (intensity == 0)
+			{
+				Graphics.Blit (source, destination);
+				return;
+			}
+	 
+			material.SetFloat("_bwBlend", intensity);
+			Graphics.Blit (source, destination, material);
+		}
+	}
+
+}
